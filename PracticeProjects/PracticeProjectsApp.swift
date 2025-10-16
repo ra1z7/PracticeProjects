dabaaaf40edcce2e5e2bb8bd73bccf4ae20b49a0
ra1z <purnamaanrai7@gmail.com>
2025-10-16 20:18:44 +0545

    Initial Commit

---
+++ PracticeProjects/PracticeProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeProjectsApp.swift
+//  PracticeProjects
+//
+//  Created by Purnaman Rai (College) on 16/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PracticeProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
