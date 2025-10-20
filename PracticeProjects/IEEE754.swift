982b562f92cbe97a2c5697cdcbb29e8de311266c
ra1z <purnamaanrai7@gmail.com>
2025-10-20 14:30:53 +0545

    Made Delete Button's Behaviour Convenient. Animation To Be Fixed

--- PracticeProjects/IEEE754.swift
+++ PracticeProjects/IEEE754.swift
@@ -45,19 +45,21 @@ struct IEEE754: View {
     @State private var inputToConvert = ""
     
     @State private var beforeConversionText = ""
+    @State private var beforeConversionTextPosition = 0.0
+    @State private var beforeConversionTextSize = 25.0
+    
     @State private var convertButtonPressed = false
     
     var body: some View {
         VStack {
             ZStack {
                 Text(beforeConversionText)
-                    .font(.system(size: convertButtonPressed ? 15 : 25, weight: .semibold, design: .monospaced))
+                    .font(.system(size: beforeConversionTextSize, weight: .semibold, design: .monospaced))
                     .foregroundStyle(.secondary)
+                    .offset(y: beforeConversionTextPosition)
                     .contentTransition(.numericText())
-                    .offset(y: convertButtonPressed ? -50 : 0)
                     .onTapGesture {
                         withAnimation {
-                            convertButtonPressed = false
                             inputToConvert = beforeConversionText
                             beforeConversionText = ""
                         }
@@ -107,7 +109,11 @@ struct IEEE754: View {
                 }
                 
                 Button {
-                    beforeConversionText = ""
+                    if convertButtonPressed && !beforeConversionText.isEmpty {
+                        inputToConvert = ""
+                        beforeConversionText = ""
+                        convertButtonPressed = false
+                    }
                     
                     if inputToConvert.count > 0 {
                         inputToConvert.removeLast()
@@ -123,8 +129,11 @@ struct IEEE754: View {
             HStack {
                 Button {
                     withAnimation {
-                        convertButtonPressed = true
                         beforeConversionText = inputToConvert
+                        beforeConversionTextPosition = -50.0
+                        beforeConversionTextSize = 15.0
+                        
+                        convertButtonPressed = true
                     }
                     
                     // Temporary Value To Show
@@ -141,21 +150,10 @@ struct IEEE754: View {
                 } label: {
                     Text("CONVERT")
                         .font(.system(size: 16, weight: .semibold, design: .monospaced))
-                        .frame(width: 132, height: 30)
+                        .frame(width: 212, height: 30)
                 }
                 .buttonStyle(.borderedProminent)
                 .applyCustomKeyStyle(ofColor: .green)
-                
-                Button {
-                    inputToConvert = ""
-                    beforeConversionText = ""
-                } label: {
-                    Image(systemName: "trash")
-                        .fontWeight(.semibold)
-                        .frame(width: 50, height: 30)
-                }
-                .buttonStyle(.borderedProminent)
-                .applyCustomKeyStyle(ofColor: .red)
             }
         }
     }
