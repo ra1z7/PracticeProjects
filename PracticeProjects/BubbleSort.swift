957470e155fbf7440562a5d8a02a244c2e8fde81
ra1z <purnamaanrai7@gmail.com>
2025-10-29 07:25:29 +0545

    Add Sorting State and Shuffle Button

--- PracticeProjects/BubbleSort.swift
+++ PracticeProjects/BubbleSort.swift
@@ -10,19 +10,13 @@ import SwiftUI
 struct ColoredNumber: View, Identifiable {
     let id = UUID()
     let number: Int
-    let color = Color(
-        red: .random(in: 0...1),
-        green: .random(in: 0...1),
-        blue: .random(in: 0...1)
-    )
     
     var body: some View {
         Text("\(number)")
             .frame(width: 20, height: 20)
             .padding(10)
-            .background(color.gradient.opacity(0.8))
-            .foregroundStyle(.white)
-            .shadow(radius: 10)
+            .background(.secondary.opacity(0.2))
+            .foregroundStyle(.primary)
             .clipShape(.rect(cornerRadius: 15))
             .font(.system(size: 12, weight: .bold, design: .monospaced))
     }
@@ -30,42 +24,66 @@ struct ColoredNumber: View, Identifiable {
 
 struct BubbleSort: View {
     @State private var numbersToSort = [29, 10, 14, 37, 13, 47, 1]
+    @State private var sortingState = ""
     
     var body: some View {
+        Text(sortingState)
+            .font(.system(size: 16, design: .monospaced))
+            .foregroundStyle(.secondary)
+            .frame(width: 170, height: 50)
+            .contentTransition(.numericText())
+            .animation(.default, value: sortingState)
+        
         HStack {
             ForEach(numbersToSort, id: \.self) { number in
                 ColoredNumber(number: number)
             }
         }
         
-        Button("Sort") {
-            Task {
-                var allNumbersSorted = false
-                
-                while !allNumbersSorted {
-                    var sortedCount = 0
+        HStack {
+            Button("Sort") {
+                Task {
+                    sortingState = "Sorting..."
+                    var allNumbersSorted = false
                     
-                    for i in 0..<numbersToSort.count - 1 {
-                        if numbersToSort[i] < numbersToSort[i + 1] {
-                            sortedCount += 1
-                            continue
-                        } else {
-                            let temp = numbersToSort[i]
-                            withAnimation {
-                                numbersToSort[i] = numbersToSort[i + 1]
-                                numbersToSort[i + 1] = temp
+                    while !allNumbersSorted {
+                        var sortedCount = 0
+                        
+                        for i in 0..<numbersToSort.count - 1 {
+                            if numbersToSort[i] < numbersToSort[i + 1] {
+                                sortedCount += 1
+                                continue
+                            } else {
+                                let temp = numbersToSort[i]
+                                withAnimation {
+                                    numbersToSort[i] = numbersToSort[i + 1]
+                                    numbersToSort[i + 1] = temp
+                                }
+                                
+                                try? await Task.sleep(for: .seconds(1))
                             }
-                            
-                            try? await Task.sleep(for: .seconds(1))
                         }
-                    }
-                    
-                    if sortedCount == numbersToSort.count - 1 {
-                        allNumbersSorted = true
+                        
+                        if sortedCount == numbersToSort.count - 1 {
+                            sortingState = "Sorting Complete"
+                            allNumbersSorted = true
+                        }
                     }
                 }
             }
+            .buttonStyle(.borderedProminent)
+            .tint(.blue)
+            
+            Button("Shuffle") {
+                withAnimation {
+                    sortingState = ""
+                    numbersToSort.shuffle()
+                }
+            }
+            .buttonStyle(.bordered)
+            .tint(.orange)
         }
+        .padding()
     }
 }
 
